28ac241247ddc145f560b5a46030d73e99aa6653
Jamie Foster <jamiefoster564@gmail.com>
2022-05-08 16:13:01 -0400

    added working search form and css styling on components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Search
-      </header>
+      <div className="Masthead">
+      <h1>Dictionary</h1>
+      <Dictionary />
+      </div>
+      <footer>
+        Coded by Jamie Foster <a href="https://github.com/Jamie-F42/dictionary-project-react">Github</a>
+      </footer>
     </div>
   );
 }
