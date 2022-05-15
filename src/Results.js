dcca78f5bb0411e3dad96fd4695c1400aac157b2
Jamie Foster <jamiefoster564@gmail.com>
2022-05-15 18:06:21 -0400

    added phonetics audio

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props){
     if (props.results) {
     return (
     <div className="Results">
-            <h2>{props.results.word}<span className="phonetic"> {props.results.phonetic}</span></h2>
+            <h2>{props.results.word}<span className="phonetic"> {props.results.phonetic} <a href={props.results.phonetics[0].audio} target="_blank" className="Phonetics">🔊</a> </span></h2>
             {props.results.meanings.map(function (meaning, index) {
                 return (
                   <div key={index}>
