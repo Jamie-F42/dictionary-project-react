0bf0e8487e60ba8e8f5bce6e70faf73fa4186eb5
Jamie Foster <jamiefoster564@gmail.com>
2022-05-17 01:06:08 -0400

    added photos component and api and added styling

---
+++ src/Photos.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+    if (props.photos){
+    return (
+        <div className="Photos">
+            {props.photos.map(function(photo, index) {
+                return (
+                <div className="PhotoLoop" key={index}>
+                    <a href={photo.src.original} target="_blank" rel="noopener">
+                 <img src={photo.src.tiny} className="img-fluid" />
+                 </a> 
+                 </div>
+                );  
+            })}
+        </div>
+    );
+} else {
+    return null;
+}
+}
